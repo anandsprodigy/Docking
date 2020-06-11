e22701f36bfb9a064441ab62641d7fde48b30af3
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-06-11 20:37:29 +0530

    Create readOnly_variable.sh

---
+++ Sample_bash_scripts/readOnly_variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Author: Anand Kulkarni
+#Purpose: Read Only variable declaration and usage
+
+
+NAME="Anand Kulkarni"
+readonly Name
+Name="Other Name"
+
+
+#when user marked as readonly then it cannot be  modified
