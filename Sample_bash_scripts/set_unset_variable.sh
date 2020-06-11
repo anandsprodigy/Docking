c256be0cd18ae807f37e132aae2243ea1fa90d72
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-06-11 20:44:22 +0530

    Create set_unset_variable.sh

---
+++ Sample_bash_scripts/set_unset_variable.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#Author Anand Kulkarni
+#Purpose set and unset variables
+
+NAME="Anand"
+
+echo $NAME
+
+unset NAME
+echo $NAME
+
+
+#When unset/delete variable then it will remove variable from list of variable
+
