8a82abca380f021f5d7ca81de7124a58c8117371
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-06-11 21:05:15 +0530

    Create special_shell_variables.sh

---
+++ Sample_bash_scripts/special_shell_variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Author Anand Kulkarni
+#Purpose Special variables
+
+echo "'\$$' --> Shell program PID: $$";
+echo "'\$0' --> File Name: $0";
+echo "'\$1' --> First argument $1";
+echo "'\$2' --> Second argument $2";
+echo "'\$#' --> Total number of argument: $#";
+echo "'\$*' --> All argument in quoted: $*";
+echo "'\$@' --> All argument are individually quoted: $@";
