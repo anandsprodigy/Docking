f4976b1c950bf58e6b54f9f0b16695d0fc62655d
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2020-06-29 21:43:57 +0530

    Create variable_script.sh

---
+++ Sample_bash_scripts/variable_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Author Anand Kulkarni
+#Purpose Variable script
+
+name="Anand Kulkarni";
+
+echo "My name is $name";
+
+#Note that there must be no spaces around the '=' sign: VAR=value works; VAR = value doesn't work.
+
+echo "${name} in quotes";
